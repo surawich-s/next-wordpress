c88cd85cb7b3eadfcf5aab7e6a3846a812ab3219
Surawich Sittisarn <surawich.sit@gmail.com>
2021-07-29 00:09:29 +0700

    add heropost and responsive index

---
+++ components/Footer.js
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <footer className="flex items-center justify-center w-full h-24 border-t text-black">
+      <a
+        className="flex items-center justify-center"
+        href="/"
+        rel="noopener noreferrer"
+      >
+        WordPress + React
+      </a>
+    </footer>
+  );
+}
