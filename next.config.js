a07c91e45c67bf8bc91b7c36c0df8e4fa43aae78
Surawich Sittisarn <surawich.sit@gmail.com>
2021-08-25 01:12:30 +0700

    update fetching logic

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 module.exports = {
   env: {
-    API: "https://ec2-54-169-219-87.ap-southeast-1.compute.amazonaws.com/wp-json/wp/v2/",
+    API: "http://ec2-54-169-219-87.ap-southeast-1.compute.amazonaws.com/wp-json/wp/v2/",
   },
   images: {
-    domains: [
-      "ec2-54-169-219-87.ap-southeast-1.compute.amazonaws.com",
-      "images.unsplash.com",
-    ],
+    domains: ["ec2-54-169-219-87.ap-southeast-1.compute.amazonaws.com"],
   },
 };
